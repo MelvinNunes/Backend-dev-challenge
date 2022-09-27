8d274158b886ca6ae1b98789f27db8d5c0fe7bfa
Melvin Nunes <Melvinnunes14@gmail.com>
2022-09-28 01:52:06 +0200

    Versão final - Beta-Test

---
+++ src/main/java/com/melvin/model/Pais.java
@@ -0,0 +1,24 @@
+package com.melvin.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Pais {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String nome;
+    @Column(nullable = false)
+    private String capital;
+    @Column(nullable = false)
+    private String regiao;
+    @Column(nullable = false)
+    private String subRegiao;
+    @Column(nullable = false)
+    private String area;
+}
---
+++ src/main/java/com/melvin/services/PaisService.java
@@ -0,0 +1,50 @@
+package com.melvin.services;
+
+import com.melvin.model.Pais;
+import com.melvin.repo.PaisRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class PaisService {
+    private final PaisRepository paisRepository;
+
+    @Autowired
+    public PaisService(PaisRepository paisRepository) {
+        this.paisRepository = paisRepository;
+    }
+
+    // Cria um país
+    public Pais createPais(Pais pais){
+        return paisRepository.save(pais);
+    }
+    // Atualiza/modifica um país
+    public  Pais updatePais(Pais pais){
+        return paisRepository.save(pais);
+    }
+    // Elimina um pais pelo id
+    public void deletePais(Long id){
+        paisRepository.deleteById(id);
+    }
+    // Lista todos os paises
+    public List<Pais> getPaises(){
+        return paisRepository.findAll();
+    }
+    // Procura um pais pelo ID
+    public Pais getContactById(Long id) throws Exception {
+        return paisRepository.findById(id).orElseThrow(()-> new Exception("Pais com "+id+" não foi encontrado!"));
+    }
+    // Ordena os paises por qualquer propriedade
+    public List<Pais> getPaisesOrderedBy (String value){
+        return paisRepository.findAll(Sort.by(Sort.Direction.ASC, value));
+    }
+    // Conta quantos países existem
+    public Long getTotalPaises(){
+        return paisRepository.count();
+    }
+}
